4a28cf15701c3041e509300df1443f70d52bfa53
marionegri <mariorafaelmarranegri@gmail.com>
2019-07-16 17:45:06 -0300

    alteracao

--- aula5/variaveis-puppet.pp
+++ aula5/variaveis-puppet.pp
@@ -1,6 +1,6 @@
 $dns1 = '8.8.8.8'
 $dns2 = '8.8.4.4'
-$arquivo = '/etc/resolv.conf'
+$arquivo = '/run/resolvconf/resolv.conf'
 $conteudo = "nameserver ${dns1}\nnameserver ${dns2}"
 
 file { $arquivo:
